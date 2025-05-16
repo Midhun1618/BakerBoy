f70e90ed56c286f0d7ebcd74d2b8022fb538a547
midhun1618 <midhumidhun342@gmail.com>
2025-05-16 10:35:18 +0530

    initialized items

--- app/src/main/java/com/voxcom/bakerboy/MainActivity.kt
+++ app/src/main/java/com/voxcom/bakerboy/MainActivity.kt
@@ -3,10 +3,21 @@ package com.voxcom.bakerboy
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    lateinit var muffin: CardView
+    lateinit var cupcake: CardView
+    lateinit var donut: CardView
+    lateinit var macaron: CardView
+    lateinit var bananabread: CardView
+    lateinit var cinnamonroll: CardView
+    lateinit var pastry: CardView
+    lateinit var redvelvet: CardView
+    lateinit var cheesecake: CardView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +27,15 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        muffin = findViewById(R.id.muffin)
+        cupcake = findViewById(R.id.cupcake)
+        donut = findViewById(R.id.donut)
+        cinnamonroll = findViewById(R.id.cinnamonroll)
+        bananabread = findViewById(R.id.bananabread)
+        macaron = findViewById(R.id.macaron)
+        pastry = findViewById(R.id.pastry)
+        redvelvet = findViewById(R.id.redvelvet)
+        cheesecake = findViewById(R.id.cheesecake)
+
     }
 }
