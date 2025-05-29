9b51a8feeac44f746441061fcd40d757555aa6b4
Midhun1618 <midhumidhun34@gmail.com>
2025-05-29 13:56:27 +0530

    check

--- app/src/main/java/com/voxcom/bakerboy/MainActivity.kt
+++ app/src/main/java/com/voxcom/bakerboy/MainActivity.kt
@@ -42,6 +42,7 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val abc ="abc"
         val data = getSharedPreferences("live_data", MODE_PRIVATE)
         val edit = data.edit()
         edit.putInt("muffin",0)
